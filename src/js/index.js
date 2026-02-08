14ddbf9eb2d1fa66036ce52258f2750b2e4c8d6f
JsuerT <johanna.suermann@iu-study.org>
2026-02-08 11:17:01 +0100

    Erster Upload: Grundgerüst Nav

---
+++ src/js/index.js
@@ -0,0 +1,6 @@
+const hamMenu = document.querySelector('.ham-menu');
+const offScreenMenu = document.querySelector('.offscreenmenu');
+hamMenu.addEventListener('click', () => {
+    hamMenu.classList.toggle('active');
+    offScreenMenu.classList.toggle('active');
+})
