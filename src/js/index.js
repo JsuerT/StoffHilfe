1e66e95af68086ba677b9fdf21e69443c46b5050
JsuerT <johanna.suermann@iu-study.org>
2026-02-14 21:24:50 +0100

    section: WillkommensText Mobile

--- src/js/index.js
+++ src/js/index.js
@@ -5,3 +5,25 @@ hamMenu.addEventListener('click', () => {
     hamMenu.classList.toggle('active');
     offScreenMenu.classList.toggle('active');
 })
+
+//Index: 2 Section: Willkommenstext, mehr anzeigen
+const beschreibungMehrInfos = document.querySelector('.beschreibungMehrInfo');
+const beschreibungWenigerInfos = document.querySelector('.beschreibungWenigerInfo');
+const beschreibungInhalt = document.querySelector('.beschreibungInhalt');
+const beschreibungEnde = document.querySelector('.beschreibungEnde');
+
+beschreibungMehrInfos.addEventListener('click', () => {
+    beschreibungInhalt.classList.toggle('active');
+    beschreibungEnde.classList.toggle('active');
+    beschreibungWenigerInfos.classList.toggle('active');
+
+    beschreibungMehrInfos.classList.toggle('hidden');
+})
+
+beschreibungWenigerInfos.addEventListener('click', () => {
+    beschreibungInhalt.classList.toggle('active');
+    beschreibungEnde.classList.toggle('active');
+    beschreibungWenigerInfos.classList.toggle('active');
+
+    beschreibungMehrInfos.classList.toggle('hidden')
+})
